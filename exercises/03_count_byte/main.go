ac55772fd81b11cd1be6363496758e40255bba3c
Sp0ozy <imolchanov210@gmail.com>
2021-12-19 19:30:33 +0200

    count byyte fix

--- exercises/03_count_byte/main.go
+++ exercises/03_count_byte/main.go
@@ -18,16 +18,15 @@ func CountByte(s string, b byte) int {
 	return a
 }
 func main() {
-	r := bufio.NewReader(os.Stdin)
-	scanner := bufio.NewScanner(r)
 	fmt.Print("Enter a string: ")
-	scanner.Scan()
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println(scanner.Scan(), scanner.Err(), scanner.Text())
 	s := scanner.Text()
-	fmt.Print("Enter a byte: ")
+	fmt.Println("Enter a byte: ")
+	fmt.Println(scanner.Scan(), scanner.Err(), scanner.Text())
+	bs := scanner.Text()
 	var b byte
-	fmt.Fscanf(r, "%c", &b)
-	fmt.Printf("s = %[1]q\nb = %[2]c (%[2]d)", s, b)
-	fmt.Print(" => contains ")
-	fmt.Printf("%[2]c", s, b, " ")
-	fmt.Println(" ", CountByte(s, b), "times")
+	fmt.Sscan(bs, &b)
+	fmt.Println(b)
+	fmt.Println(CountByte(s, b), "times")
 }
