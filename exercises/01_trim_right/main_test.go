3f54c0a96181152668707dcfe120ae5168cb82f7
Андрей Черемных <andrejceremnih@MacBook-Air-Andrej.local>
2021-12-20 23:32:54 +0200

    01_trim_right done

---
+++ exercises/01_trim_right/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestTrimRight(t *testing.T) {
+	for _, tc := range []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{"empty", "", ""},
+		{"single space", " ", ""},
+		{"nothing to trim", "abc", "abc"},
+		{"trim set", " \n\r\t", ""},
+		{"double trim set", "  \n\n\r\r\t\t", ""},
+		{"mixed trim set", " \t \n \t \r \t \n", ""},
+		{"trims", "abc\t \n \r    ", "abc"},
+		{"keeps head", "   abc", "   abc"},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := TrimRight(tc.input); got != tc.want {
+				t.Errorf("got = %q, want = %q", got, tc.want)
+			}
+		})
+	}
+}
