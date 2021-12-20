cf8b9d3b5198f093fab5777ab713bf635fff68ed
Alina Aniscenko <alina065b@gmail.com>
2021-12-20 21:23:40 +0200

    updated palindrom

--- exercises/02_check_palindrom/02_check_palindrom.go
+++ exercises/02_check_palindrom/02_check_palindrom.go
@@ -1,13 +1,24 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func IsPalindrom(s string) bool {
 	r := []rune(s)
-	i, j := 0, len(r)-1
-	for ; i < j; i, j = i+1, j-1 {
-		if r[i] != r[j] {
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j+1 {
+		if r[i] == r[j] {
 			return false
 		}
 	}
-
 	return true
+}
+func main() {
+	fmt.Println("Program checks whether a given string is a palindrome")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Printf("%v\n", IsPalindrom(text))
 }
