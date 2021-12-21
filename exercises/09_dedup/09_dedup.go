e7dde62cc6853ad0e5c6298abe2cb684366d3d09
Alina Aniscenko <alina065b@gmail.com>
2021-12-21 21:19:20 +0200

    dedup program, but it  is incorrect now

---
+++ exercises/09_dedup/09_dedup.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Dedup(s string) string {
+	var r []rune
+	var a rune
+	for _, w := range s {
+		if rune(w) != a {
+			r = append(r, w)
+		}
+	}
+	return string(r)
+}
+
+func main() {
+	fmt.Println(`Program removes duplicates by dropping
+	consequent characters if they are`)
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Enter the string:")
+	scanner.Scan()
+	s := scanner.Text()
+	fmt.Println(Dedup(s))
+}
