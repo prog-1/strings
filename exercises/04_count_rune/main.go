fee7d3d923c3171e86faeea176fd890e3fbe7647
Андрей Черемных <andrejceremnih@MacBook-Air-Andrej.local>
2021-12-30 22:25:53 +0200

    04_count_rune done 2

--- exercises/04_count_rune/main.go
+++ exercises/04_count_rune/main.go
@@ -7,28 +7,34 @@ import (
 )
 
 func CountRune(s string, r rune) int {
-	var i int
-	j := []rune(s)
-	for a := range j {
-		if j[a] == r {
-			i++
+	var count int
+	for _, c := range s {
+		if c == r {
+			count++
 		}
 	}
-	return i
+	return count
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	scanner := bufio.NewScanner(reader)
-	fmt.Println("Enter string:")
+	fmt.Print("Enter string: ")
 	scanner.Scan()
 	s := scanner.Text()
-	fmt.Println("Enter rune:")
+	fmt.Print("Enter rune: ")
 	var r rune
 	fmt.Fscanf(reader, "%c", &r)
-	if cnt := CountRune(s, r); cnt > 0 {
-		fmt.Printf("%[1]q contains rune %[2]q (code %[2]d) %[3]d times.\n", s, r, cnt)
+	if count := CountRune(s, r); count > 0 {
+		fmt.Printf("%q contains rune %q (code %d) %d times.\n", s, r, r, count)
 	} else {
-		fmt.Printf("%[1]q does not contain rune %[2]q (code %[2]d).\n", s, r)
+		fmt.Printf("%q does not contain rune %q (code %d).\n", s, r, r)
 	}
+	// Another version:
+	//
+	// if count := CountRune(s, r); count > 0 {
+	//	   fmt.Printf("%[1]q contains rune %[2]q (code %[2]d) %[3]d times.\n", s, r, count)
+	// } else {
+	// 	   fmt.Printf("%[1]q does not contain rune %[2]q (code %[2]d).\n", s, r)
+	// }
 }
--- exercises/04_count_rune/main_test.go
+++ exercises/04_count_rune/main_test.go
@@ -4,29 +4,29 @@ import "testing"
 
 func TestCountRune(t *testing.T) {
 	for _, tc := range []struct {
-		name   string
-		input  string
-		inputr rune
-		want   int
+		name      string
+		input     string
+		inputrune rune
+		want      int
 	}{
-		{"empty", "", 101, 0},
-		{"one letter", "h", 104, 1},
-		{"two letters", "ha", 97, 1},
-		{"two-digit number", "13", 50, 0},
-		{"three-digit number", "123", 50, 1},
-		{"three-digit number", "2123", 50, 2},
-		{"one word", "generous", 110, 1},
-		{"two words", "tuc tuduc", 117, 3},
-		{"spaces left", "    tadam", 97, 2},
-		{"spaces right", "tam     ", 97, 1},
-		{"mixed", "353gefbi5", 50, 0},
-		{"mixed", "353gef,bi5.", 101, 1},
-		{"mixed", "tuk.tuk!  and?", 117, 2},
-		{"unicode", "привет!  как дела?", 1072, 2},
-		{"mixed unicode", "прiвет!  how are your дела?", 1072, 1}, // "a" in "are" is English letter
+		{"empty", "", 0, 0}, // {"empty", "", 'e', 0},
+		{"one letter", "h", 'h', 1},
+		{"two letters", "ha", 'a', 1},
+		{"two-digit number", "13", '2', 0},
+		{"three-digit number", "123", '2', 1},
+		{"four-digit number", "2123", '2', 2},
+		{"one word", "generous", 'n', 1},
+		{"two words", "tuc tuduc", 'u', 3},
+		{"spaces left", "    tadam", 'a', 2},
+		{"spaces right", "tam     ", 'a', 1},
+		{"mixed", "353gefbi5", '2', 0},
+		{"mixed", "353GEf,bi5.", 'E', 1},
+		{"mixed", "tuk.tuk!  and?", 'u', 2},
+		{"unicode", "привет! как дела?", 'а', 2},
+		{"mixed unicode", "прiвеt!  how are your дела?", 'а', 1}, // "a" in "are" is an English letter, 'a' in inputrune is a Russian letter
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			if got := CountRune(tc.input, tc.inputr); got != tc.want {
+			if got := CountRune(tc.input, tc.inputrune); got != tc.want {
 				t.Errorf("got = %q, want = %q", got, tc.want)
 			}
 		})
