d9f5320cf33044fcbc66e31d3ed3fe0f0c77d0aa
Андрей Черемных <andrejceremnih@MacBook-Air-Andrej.local>
2021-12-30 22:26:56 +0200

    05_lower_upper done 2

--- exercises/05_lower_upper/main.go
+++ exercises/05_lower_upper/main.go
@@ -4,15 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"unicode"
 )
 
 func ToLower(s string) string {
 	var r []rune
-	if len(s) >= 1 {
-		for _, i := range s {
-			r = append(r, unicode.ToLower(i))
-			i++
+	for _, c := range s {
+		if 'A' <= c && c <= 'Z' {
+			r = append(r, 'a'+c-'A')
+		} else {
+			r = append(r, c)
 		}
 	}
 	return string(r)
@@ -20,19 +20,23 @@ func ToLower(s string) string {
 
 func ToUpper(s string) string {
 	var r []rune
-	if len(s) >= 1 {
-		for _, i := range s {
-			r = append(r, unicode.ToUpper(i))
-			i++
+	for _, c := range s {
+		if 'a' <= c && c <= 'z' {
+			r = append(r, 'A'+c-'a')
+		} else {
+			r = append(r, c)
 		}
 	}
 	return string(r)
 }
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Enter string: ")
+	fmt.Print("Enter text: ")
 	scanner.Scan()
 	s := scanner.Text()
-	fmt.Println("All lower cases:", ToLower(s))
-	fmt.Println("All upper cases:", ToUpper(s))
+	fmt.Print("All lower cases: ")
+	fmt.Printf("%q -> %q\n", s, ToLower(s))
+	fmt.Print("All upper cases: ")
+	fmt.Printf("%q -> %q\n", s, ToUpper(s))
 }
--- exercises/05_lower_upper/main_test.go
+++ exercises/05_lower_upper/main_test.go
@@ -9,16 +9,19 @@ func TestToLower(t *testing.T) {
 		want  string
 	}{
 		{"empty", "", ""},
-		{"one letter", "a", "a"},
-		{"one letter", "A", "a"},
-		{"two letters", "ab", "ab"},
-		{"two letters", "AB", "ab"},
-		{"one word", "golang", "golang"},
-		{"one word", "gOlAng", "golang"},
-		{"one word", "GOLANG", "golang"},
-		{"two words", "hOliDayS aFterNOoN", "holidays afternoon"},
+		{"one lower case letter", "a", "a"},
+		{"one upper case letter", "A", "a"},
+		{"two lower case letters", "ab", "ab"},
+		{"two upper case letters", "AB", "ab"},
+		{"one word with all letters lower case", "hello", "hello"},
+		{"one word with letters both lower case and upper case", "hEllO", "hello"},
+		{"one word with all letters upper case", "HELLO", "hello"},
+		{"two words with letters both lower case and upper case", "hOliDayS aFterNOoN", "holidays afternoon"},
 		{"number", "123", "123"},
 		{"mixed", " UtrO, veCHer! ", " utro, vecher! "},
+		{"unicode", "Привет, миР!", "Привет, миР!"},           // if there is a non-English letter in the input, the program will just return it unchanged
+		{"mixed unicode", "Priвет,  Дryг.", "priвет,  Дryг."}, // "y" in "дryг" is an English letter
+
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			if got := ToLower(tc.input); got != tc.want {
@@ -35,16 +38,18 @@ func TestToUpper(t *testing.T) {
 		want  string
 	}{
 		{"empty", "", ""},
-		{"one letter", "a", "A"},
-		{"one letter", "A", "A"},
-		{"two letters", "ab", "AB"},
-		{"two letters", "AB", "AB"},
-		{"one word", "golang", "GOLANG"},
-		{"one word", "gOlAng", "GOLANG"},
-		{"one word", "GOLANG", "GOLANG"},
-		{"two words", "hOliDayS aFterNOoN", "HOLIDAYS AFTERNOON"},
+		{"one lower case letter", "a", "A"},
+		{"one upper case letter", "A", "A"},
+		{"two lower case letters", "ab", "AB"},
+		{"two upper case letters", "AB", "AB"},
+		{"one word with all letters lower case", "hello", "HELLO"},
+		{"one word with letters both lower case and upper case", "hEllO", "HELLO"},
+		{"one word with all letters upper case", "HELLO", "HELLO"},
+		{"two words with letters both lower case and upper case", "hOliDayS aFterNOoN", "HOLIDAYS AFTERNOON"},
 		{"number", "123", "123"},
 		{"mixed", " UtrO, veCHer! ", " UTRO, VECHER! "},
+		{"unicode", "Привет, миР!", "Привет, миР!"},           // if there is a non-English letter in the input, the program will just return it unchanged
+		{"mixed unicode", "Priвет,  Дryг.", "PRIвет,  ДRYг."}, // "y" in "дryг" is an English letter
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			if got := ToUpper(tc.input); got != tc.want {
